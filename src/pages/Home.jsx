9743d749bf1c5b3a0ce12a180a211d1af796b4bf
szodavid4-png <szodavid4@gmail.com>
2026-04-15 17:31:17 +0300

    Add Home component with multiple sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import Navbar from '../components/landing/Navbar';
+import HeroSection from '../components/landing/HeroSection';
+import ServicesSection from '../components/landing/ServicesSection';
+import PartnersSection from '../components/landing/PartnersSection';
+import StatsSection from '../components/landing/StatsSection';
+import GallerySection from '../components/landing/GallerySection';
+import TestimonialsSection from '../components/landing/TestimonialsSection';
+import QuoteFormSection from '../components/landing/QuoteFormSection';
+import FooterSection from '../components/landing/FooterSection';
+import FloatingCTA from '../components/landing/FloatingCTA';
+import ScrollProgress from '../components/landing/ScrollProgress';
 
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <PartnersSection />
+      <ServicesSection />
+      <StatsSection />
+
+      <GallerySection />
+      <TestimonialsSection />
+      <QuoteFormSection />
+      <FooterSection />
+      <FloatingCTA />
+      <ScrollProgress />
+    </div>
+  );
+}
