14a2b9911b476c51e3efeb75e3ee1e7f5d1eae9b
szodavid4-png <szodavid4@gmail.com>
2026-04-15 17:30:26 +0300

    Set up routing and add Toaster component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Toaster } from 'sonner';
+import Home from './pages/Home';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Toaster />
+    </Router>
+  )
+}
+
+export default App
