b39edfcd3f76e93623feb8fe62257a3d9e2ca7e4
szodavid4-png <szodavid4@gmail.com>
2026-04-16 19:18:47 +0300

    Refactor base44 client to use createClient function

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,10 +1,14 @@
-export const base44 = {
-  entities: {
-    QuoteRequest: {
-      create: async (data) => {
-        console.log('Quote request:', data);
-        return data;
-      }
-    }
-  }
-};
+import { createClient } from '@base44/sdk';
+import { appParams } from '@/lib/app-params';
+
+const { appId, token, functionsVersion, appBaseUrl } = appParams;
+
+//Create a client with authentication required
+export const base44 = createClient({
+  appId,
+  token,
+  functionsVersion,
+  serverUrl: '',
+  requiresAuth: false,
+  appBaseUrl
+});
